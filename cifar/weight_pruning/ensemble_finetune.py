205e0df5ce818e00351860c8344bc46e9146996e
lehduong <oopsxilitol@gmail.com>
2020-04-11 00:52:16 +0700

    fixed unlisted param in ensemblefinetune  of weight pruning

--- cifar/weight_pruning/ensemble_finetune.py
+++ cifar/weight_pruning/ensemble_finetune.py
@@ -36,6 +36,10 @@ parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                     help='SGD momentum (default: 0.9)')
 parser.add_argument('--weight-decay', '--wd', default=0, type=float,
                     metavar='W', help='weight decay (default: 0)')
+parser.add_argument('--schedule', type=int, nargs='+', default=[20, 30],
+                    help='Decrease learning rate at these epochs.')
+parser.add_argument('--gamma', type=float, default=0.1, 
+                    help='LR is multiplied by gamma on schedule.')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -56,6 +60,7 @@ checkpoint_paths = ['checkpoints/cifar10/resnet-110/model_best.pth.tar',
                     'prune_2/finetuned.pth.tar',
                     'prune_3/finetuned.pth.tar',
                     'prune_4/finetuned.pth.tar',
+                    'prune_5/finetuned.pth.tar',
                     ]
 
 args = parser.parse_args()
@@ -134,8 +139,8 @@ if args.cuda:
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, 
-                                              milestones=[int(0.5*args.epochs), int(0.75*args.epochs)],
-                                              gamma=0.2)
+                                              milestones=args.schedule],
+                                              gamma=args.gamma)
 lr_scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, div_factor=10,
                                                      epochs=args.epochs, steps_per_epoch=len(train_loader), pct_start=0.1,
                                                      final_div_factor=100)
